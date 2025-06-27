a53ddb9545384533428a82e9478ea806a2cd5832
Patrick Paiva <franciscopatrickp258@gmail.com>
2025-06-27 17:43:31 -0300

    arquivo task

---
+++ src/models/task.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    priority: {
+        type: String,
+        enum: ["low", "medium", "high"],
+        default: "medium"
+    },
+    //data de vencimento
+    dueDate: {
+        type: Date,
+        required: true
+    },
+   
+})
+
+const Task = mongoose.model("Task", taskSchema);
+
+export default Task;
